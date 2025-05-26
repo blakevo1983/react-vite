cf36536f6d372041f6b455650b575e07f600e1da
Blake Vo <vocongquocquang@gmail.com>
2025-05-26 16:27:21 +0700

    video 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,7 @@
+const MyComponent = () => {
+  return (
+    <div> Blake Hoc Lap Trinh Day1</div>
+  )
+}
+
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,20 @@
+
+const ThirdComponent = () => {
+
+    return(
+        <div>
+            My Third Component
+        </div>
+    )
+}
+
+const SecondComponent = () => {
+
+    return(
+        <div>
+            My Second Component
+        </div>
+    )
+}
+
+export {SecondComponent, ThirdComponent};
