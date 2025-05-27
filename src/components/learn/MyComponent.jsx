45b33cd30c8a7cf9f189ad0a0872872563d0cf1a
Blake Vo <vocongquocquang@gmail.com>
2025-05-27 15:32:35 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,21 @@
 import './style.css';
 
 const MyComponent = () => {
+  //const blake = "Quang"; //string
+  //const blake = 113; //number
+  //const blake = true; //boolean
+  //const blake = undefined;
+  //const blake = null;
+  const blake = [1, 2, 3]; //array
+  // const blake = {
+  //   name: "Blake Vo",
+  //   age: 35
+  // }
+
   return (
     <>
-    <div> Blake Hoc Lap Trinh Day1</div>
+    <div> {JSON.stringify(blake)} & Blake Hoc Lap Trinh Day1</div>
+    <div>{console.log("BLAKE")}</div>
     <div className ="child" 
         style={{borderRadius:"10px"}}
     >Bom</div>
