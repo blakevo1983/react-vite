d8fba4b17ed33ed2fcb617902b21e5ab9f8ae3f9
Blake Vo <vocongquocquang@gmail.com>
2025-05-27 15:04:16 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,15 @@
+import './style.css';
+
 const MyComponent = () => {
   return (
+    <>
     <div> Blake Hoc Lap Trinh Day1</div>
-  )
+    <div className ="child" 
+        style={{borderRadius:"10px"}}
+    >Bom</div>
+    <div className = "parent">Quang</div>
+    </>
+  );
 }
 
 export default MyComponent;
